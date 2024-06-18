411b44e2d58f7eeaeb1d949d264a2b33ceafa657
unknown <yjc533@catholic.ac.kr>
2024-06-18 16:14:38 +0900

    보드 엔티티 설정

--- src/api/boards/entities/board.entity.ts
+++ src/api/boards/entities/board.entity.ts
@@ -1,1 +1,36 @@
-export class Board {}
+import {
+    Column,
+    CreateDateColumn,
+    DeleteDateColumn,
+    Entity,
+    PrimaryGeneratedColumn,
+    Unique,
+    UpdateDateColumn,
+  } from 'typeorm';
+  
+@Entity()
+export class Board {
+    @PrimaryGeneratedColumn()
+    board_idx: number;
+
+    @Column()
+    user_idx: number;
+
+    @Column()
+    title: string;
+
+    @Column()
+    content: string;
+
+    @Column()
+    read_count: number;
+
+    @CreateDateColumn()
+    created_at: Date;
+
+    @UpdateDateColumn()
+    updated_at: Date;
+  
+    @DeleteDateColumn()
+    deleted_at: Date;
+}
